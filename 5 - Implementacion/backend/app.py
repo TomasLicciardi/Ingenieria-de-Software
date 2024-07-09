a0c7d7861b8f45ec4e18c969c6c42630402ebd44
TomasLicciardi <tomilrclicciardi@gmail.com>
2024-07-09 13:46:22 -0300

    Creando base de Backend

---
+++ 5 - Implementacion/backend/app.py
@@ -0,0 +1,13 @@
+'''
+import os
+from main import create_app
+
+app = create_app()
+
+app.app_context().push()
+
+from main import db
+
+if __name__ == '__main__':
+    db.create_all() 
+    app.run(debug=True,port=os.getenv('PORT'))'''
---
+++ 5 - Implementacion/backend/boot.sh
@@ -0,0 +1,4 @@
+source bin/activate
+python3 app.py
+
+#sudo chmod +x boot.sh <--Poner eso para dar permisos y luego en la terminal volver a ejecutar el archivo app.py
